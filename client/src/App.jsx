f6c4ca7e8cf6d6bc986bccda2f4ade335343054d
Yagrajoli <ekrajoli1235@gmail.com>
2024-11-30 21:59:18 +0545

    buiding pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import signupPage from './pages/signupPage'
+import LoginPage from './pages/LoginPage'
+import SettingPage from './pages/SettingPage'
+import ProfilePage from './pages/ProfilePage'
+import HomePage from './pages/HomePage'
 
 export default function App() {
   return (
-    <div className='text-3xl'>
-      hello world
-    </div>
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path='/' element = {<HomePage/>} />
+        <Route path='/signup' element = {<signupPage/>} />
+        <Route path='/login' element = {<LoginPage/>} />
+        <Route path='/settings' element = {<SettingPage/>} />
+        <Route path='/profile' element = {<ProfilePage/>} />
+      </Routes>
+
+    </>
   )
 }
